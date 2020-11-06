f8c6cae033c2efa4f7b232652a6ea239d5ec2a29
Adrean1983 <adrean_@hotmail.com>
2020-11-05 21:46:12 -0500

    get app ready for deployment

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
@@ -15,6 +16,12 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/v1/teamMembers', teamMembers);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFild(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
